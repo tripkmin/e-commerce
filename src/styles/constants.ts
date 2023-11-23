5abfa5d2baa1964bf316c47ced9a36cc544e43f5
tripkmin <tripkmin@gmail.com>
2023-11-23 17:49:18 +0900

    feat: Setup Initial project files

---
+++ src/styles/constants.ts
@@ -0,0 +1,21 @@
+export const color = {
+  Orange: 'hsl(26, 100%, 55%)',
+  paleOrange: 'hsl(25, 100%, 94%)',
+  veryDarkBlue: 'hsl(220, 13%, 13%)',
+  darkGraylishBlue: 'hsl(219, 9%, 45%)',
+  graylishBlue: 'hsl(220, 14%, 75%)',
+  lightGrayishBlue: 'hsl(223, 64%, 98%)',
+  white: 'hsl(0, 0%, 100%)',
+  black: 'hsl(0, 0%, 0%)',
+};
+
+export const timer = {
+  default: '0.3s',
+  fast: '0.1s',
+  slow: '0.5s',
+};
+
+export const size = {
+  mobile: '600px',
+  desktop: '1024px',
+};
